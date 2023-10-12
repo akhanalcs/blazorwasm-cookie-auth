f9818d144c8f13a58fa9b9d6a0dcfb47e348c36d
Ashish Khanal <affableashk@outlook.com>
2023-10-12 19:35:46 -0400

    Initial Commit

---
+++ BlazorWASM.BFF.Client/UserInfo.cs
@@ -0,0 +1,7 @@
+namespace BlazorWASM.BFF.Client;
+
+public class UserInfo
+{
+    public required string UserId { get; set; }
+    public required string Email { get; set; }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# blazorwasm-cookie-auth
-Placeholder
